d04d05fd3b423eeab8f1366d619bdaf44a70e245
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2021-10-29 14:55:57 -0300

    refactor: input password type

--- src/Components/Login/index.tsx
+++ src/Components/Login/index.tsx
@@ -46,6 +46,7 @@ export const Login = () => {
           error={errors.email?.message}
         />
         <Input
+          type="password"
           placeholder="Senha"
           register={register}
           text={"password"}
