9536b79a01b154a1d1ef012870900c6e64fa5a09
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2021-10-29 10:50:05 -0300

    feat: created interfaces to import

---
+++ src/Interfaces/ProvidersProps/index.ts
@@ -0,0 +1,6 @@
+import { ReactNode } from "react";
+
+interface ProvidersTypeProps {
+    children: ReactNode;
+}
+export default ProvidersTypeProps;
---
+++ src/Interfaces/UserData/index.ts
@@ -0,0 +1,5 @@
+interface UserData {
+    email: string;
+    password: string;
+}
+export default UserData;
