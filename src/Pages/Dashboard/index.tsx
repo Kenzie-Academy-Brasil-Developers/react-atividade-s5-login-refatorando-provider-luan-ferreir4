9cac5e2e138b090097f1f6ea78b254a56bdd61ad
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2021-10-29 08:58:12 -0300

    feat

---
+++ src/Pages/Dashboard/index.tsx
@@ -0,0 +1,13 @@
+import { Logout } from "../../Components/Logout";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <div>
+        Bem vindo:
+        <Logout />
+      </div>
+    </div>
+  );
+};
+export default Dashboard;
---
+++ src/Pages/Home/index.tsx
@@ -0,0 +1,10 @@
+import { Login } from "../../Components/Login";
+
+const Home = () => {
+    return(
+        <div>
+        <Login/>
+        </div>
+    )
+}
+export default Home;
